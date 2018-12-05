3639f2f3c25d2a6cac449285f9129341ee91c7f0
Deyvisson Rodrigues <deyvisson.rodrigues@acad.pucrs.br>
2018-12-05 00:43:46 -0200

    Feito meu app e abastecimentos

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String args[]){
+        System.out.println("Hello World cara.");
+
+        Abastecimento ab = new Abastecimento("ABC-132","Gasolina",12.5,3.98,20);
+
+        System.out.println(ab);
+    }
+}
