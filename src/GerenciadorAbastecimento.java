ebdbea2deb70bbcef55ae02266f2dc2449cde12f
PedroHenrique98 <pedro.medeiros.001@acad.pucrs.br>
2018-12-05 01:37:15 -0200

    lista abastecimentos de um automovel

--- src/GerenciadorAbastecimento.java
+++ src/GerenciadorAbastecimento.java
@@ -9,18 +9,32 @@ public class GerenciadorAbastecimento {
     }
 
     public void solicitarAbastecimento(Automovel auto, String tipoc, double odometroa, double lit, double precol) {
+        auto.setOdometro(odometroa);
         Abastecimento ab = new Abastecimento(auto.getPlacaVeiculo(), tipoc, odometroa, lit, precol);
         abastecimentos.add(ab);
     }
 
     public String toString() {
         String str = "[";
-        for(Abastecimento abast: abastecimentos) {
+        for(Abastecimento abast : abastecimentos) {
             str = str + abast.toString();
             str = str + ", ";
         }
         str = str.substring(0,str.length()-2);
         return str + "]";
+    }
+
+    public List<Abastecimento> getAbastecimentosAutomovel(String placa){
+        List<Abastecimento> listaAbAutomovel = new ArrayList<>();
+        if(abastecimentos.isEmpty()) {
+            return null;
+        }
+        for (Abastecimento abast : abastecimentos) {
+            if(abast.getPlacaAutomovel().equals(placa)) {
+                listaAbAutomovel.add(abast);
+            }
+        }
+        return listaAbAutomovel;
     }
 
 }
